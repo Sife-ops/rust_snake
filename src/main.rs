ca07f532a01465cea3ff400bf0fd72929a8278e6
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-12-20 07:51:51 -0800

    collision

--- src/main.rs
+++ src/main.rs
@@ -58,23 +58,36 @@ impl Snake {
 
     fn new() -> Self {
         Snake {
-            body: Point::new(3,3),
+            body: Point::new(3, 3),
             facing: Direction::Right,
+        }
+    }
+
+    fn head(&self) -> Point {
+        self.body
+    }
+
+    fn advance(&mut self) {
+        match self.facing {
+            x @ Direction::Up => self.body = self.body.transform(x),
+            x @ Direction::Down => self.body = self.body.transform(x),
+            x @ Direction::Left => self.body = self.body.transform(x),
+            x @ Direction::Right => self.body = self.body.transform(x),
         }
     }
 }
 
-use std::io::Stdout;
-use std::time::{Duration, Instant};
 use crossterm::terminal::size;
 use rand::Rng;
+use std::io::{Stdout, stdout};
+use std::time::{Duration, Instant};
 
 struct Game {
     stdout: Stdout,
-    term_size: (u16,u16),
+    term_size: (u16, u16),
     width: u16,
     height: u16,
-    snake: Snake
+    snake: Snake,
 }
 
 impl Game {
@@ -88,10 +101,32 @@ impl Game {
         }
     }
 
+    fn hit_wall(&self) -> bool {
+        let p = self.snake.head();
+        if p.x < 1 || p.x > self.width - 1 || p.y < 1 || p.y > self.height - 1 {
+            return true;
+        }
+        return false;
+    }
+
+    fn run(&mut self) {
+        let mut game_over = false;
+        while !game_over {
+            let interval = Duration::from_millis(1000);
+            let now = Instant::now();
+            while now.elapsed() < interval {
+                //;
+            }
+            if self.hit_wall() {
+                game_over = true;
+            }
+            println!("{:?}", self.snake.body);
+        }
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    Game::new(stdout(), 10, 10).run();
 }
 
 #[cfg(test)]
@@ -100,30 +135,70 @@ mod tests {
 
     #[test]
     fn test_point_transform_up() {
-        let p = Point::new(3,3);
+        let p = Point::new(3, 3);
         let p = p.transform(Direction::Up);
-        assert_eq!(p, Point::new(3,2));
+        assert_eq!(p, Point::new(3, 2));
     }
 
     #[test]
     fn test_point_transform_down() {
-        let p = Point::new(3,3);
+        let p = Point::new(3, 3);
         let p = p.transform(Direction::Down);
-        assert_eq!(p, Point::new(3,4));
+        assert_eq!(p, Point::new(3, 4));
     }
 
     #[test]
     fn test_point_transform_left() {
-        let p = Point::new(3,3);
+        let p = Point::new(3, 3);
         let p = p.transform(Direction::Left);
-        assert_eq!(p, Point::new(2,3));
+        assert_eq!(p, Point::new(2, 3));
     }
 
     #[test]
     fn test_point_transform_right() {
-        let p = Point::new(3,3);
+        let p = Point::new(3, 3);
         let p = p.transform(Direction::Right);
-        assert_eq!(p, Point::new(4,3));
+        assert_eq!(p, Point::new(4, 3));
+    }
+
+    #[test]
+    fn test_snake_advance_up() {
+        let mut s = Snake::new();
+        let mut p = s.body.clone();
+        p.y = p.y - 1;
+        s.facing = Direction::Up;
+        s.advance();
+        assert_eq!(s.body, p);
+    }
+
+    #[test]
+    fn test_snake_advance_down() {
+        let mut s = Snake::new();
+        let mut p = s.body.clone();
+        p.y = p.y + 1;
+        s.facing = Direction::Down;
+        s.advance();
+        assert_eq!(s.body, p);
+    }
+
+    #[test]
+    fn test_snake_advance_left() {
+        let mut s = Snake::new();
+        let mut p = s.body.clone();
+        p.x = p.x - 1;
+        s.facing = Direction::Left;
+        s.advance();
+        assert_eq!(s.body, p);
+    }
+
+    #[test]
+    fn test_snake_advance_right() {
+        let mut s = Snake::new();
+        let mut p = s.body.clone();
+        p.x = p.x + 1;
+        s.facing = Direction::Right;
+        s.advance();
+        assert_eq!(s.body, p);
     }
 
 }
